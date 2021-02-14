be53d43dc9ef38125ffd253bdf8785a886b2664b
jensengbg-sanel-jasarevic <55239936+jensengbg-sanel-jasarevic@users.noreply.github.com>
2021-02-14 01:26:34 +0100

    Test case 8/8

---
+++ src/components/Review.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="">
+
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'Review',
+ 
+
+  
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Reviews.vue
+++ src/views/Reviews.vue
@@ -1,4 +1,20 @@
 <template>
-  <div class="about">
-  </div>
+    <div class="about">
+    <PreviousEvents  />
+  
+    </div>
 </template>
+
+<script>
+import PreviousEvents from '@/components/PreviousEvents.vue'
+// @ is an alias to /src
+
+export default {
+  name: 'Review',
+
+  components: {
+    PreviousEvents
+  }
+  
+}
+</script>
